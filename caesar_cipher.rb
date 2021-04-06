e582e2accb819c3c9e0242d029f7afd9bc370987
Jakub Cembala <cembala.jakub@gmail.com>
2021-04-06 09:37:36 +0200

    added file with Caesar cipher method

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+DOWNCASE_RANGE = ("a".."z")
+UPPERCASE_RANGE = ("A".."Z")
+
+def caesar_cipher(string, shift_factor)
+  
+  shifted = string.chars.map do |letter| 
+    if UPPERCASE_RANGE.include?(letter)
+      ((letter.ord + shift_factor - 65) % 26) + 65
+    elsif DOWNCASE_RANGE.include?(letter)
+      ((letter.ord + shift_factor - 97) % 26) + 97
+    else
+      letter
+    end
+  end
+  shifted.map { |char| char = char.chr }.join
+end
+
+puts caesar_cipher("This is secret! Shhhh...", 5)
